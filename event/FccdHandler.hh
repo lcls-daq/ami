5a09cc7e166e0626472678cd4e2cc8efd24afca6
caf <none@example.com>
2010-06-16 08:51:33 +0000

    remove dark pixels from FCCD image

--- event/FccdHandler.hh
+++ event/FccdHandler.hh
@@ -1,16 +1,29 @@
 #ifndef Ami_FccdHandler_hh
 #define Ami_FccdHandler_hh
 
-#include "ami/event/FrameHandler.hh"
-#include "pdsdata/fccd/FccdConfigV1.hh"
+#include "ami/event/EventHandler.hh"
+#include "pdsdata/xtc/DetInfo.hh"
+#include "pdsdata/fccd/FccdConfigV2.hh"
 
 namespace Ami {
-  class FccdHandler : public FrameHandler {
+  class EntryImage;
+
+  class FccdHandler : public EventHandler {
   public:
-    FccdHandler(const Pds::DetInfo& info) :
-      FrameHandler(info,
-		   Pds::FCCD::FccdConfigV1::Column_Pixels,
-		   Pds::FCCD::FccdConfigV1::Row_Pixels) {}
+    FccdHandler(const Pds::DetInfo& info);
+    ~FccdHandler();
+  public:
+    unsigned     nentries() const;
+    const Entry* entry(unsigned) const;
+    void         reset();
+  private:
+    void _calibrate(const void* payload, const Pds::ClockTime& t);
+    void _configure(const void* payload, const Pds::ClockTime& t);
+    void _event    (const void* payload, const Pds::ClockTime& t);
+    void _damaged  ();
+  private:
+    FccdHandler(const Pds::DetInfo& info, const EntryImage*);
+    EntryImage* _entry;
   };
 };
 
