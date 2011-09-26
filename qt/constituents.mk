28ca173a5c5b435c2fdd91ca839e07ffcada81de
jackp <none@example.com>
2011-09-26 18:33:51 +0000

    Mon Sep 26 11:29:46 PDT 2011 Added fexamp data library

--- qt/constituents.mk
+++ qt/constituents.mk
@@ -45,7 +45,7 @@ tgtlibs_offline_ami += pdsdata/cspaddata pdsdata/lusidata pdsdata/appdata
 tgtlibs_offline_ami += ami/service ami/data ami/server ami/client ami/event ami/app ami/amiqt
 tgtlibs_offline_ami += $(qt_libs)
 
-datalibs := pdsdata/xtcdata pdsdata/opal1kdata pdsdata/pulnixdata pdsdata/camdata pdsdata/pnccddata pdsdata/evrdata pdsdata/acqdata pdsdata/controldata pdsdata/princetondata pdsdata/ipimbdata pdsdata/encoderdata pdsdata/fccddata pdsdata/lusidata pdsdata/cspaddata pdsdata/xampsdata
+datalibs := pdsdata/xtcdata pdsdata/opal1kdata pdsdata/pulnixdata pdsdata/camdata pdsdata/pnccddata pdsdata/evrdata pdsdata/acqdata pdsdata/controldata pdsdata/princetondata pdsdata/ipimbdata pdsdata/encoderdata pdsdata/fccddata pdsdata/lusidata pdsdata/cspaddata pdsdata/xampsdata pdsdata/fexampdata
 tgtlibs_blviewer := $(datalibs) pdsapp/configdb pdsapp/configdbg
 tgtlibs_blviewer += ami/service ami/data ami/server ami/client ami/amiqt
 tgtlibs_blviewer += $(qt_libs)
