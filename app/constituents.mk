ebab196b223f56e68a4b9c1125aa5f0036058269
philiph <none@example.com>
2011-10-17 21:10:30 +0000

    add fexamp methods to monitoring

--- app/constituents.mk
+++ app/constituents.mk
@@ -22,6 +22,7 @@ tgtlibs_ami += pdsdata/evrdata pdsdata/encoderdata
 tgtlibs_ami += pdsdata/gsc16aidata
 tgtlibs_ami += pdsdata/controldata pdsdata/epics 
 tgtlibs_ami += pdsdata/cspaddata pdsdata/lusidata pdsdata/appdata
+tgtlibs_ami += pdsdata/fexampdata
 tgtlibs_ami += ami/service ami/data ami/server ami/event ami/client ami/app
 tgtlibs_ami += qt/QtCore
 
