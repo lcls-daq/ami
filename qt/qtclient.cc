f37d55168231b0ab11fea2df086285141926c1ae
jbarrera <none@example.com>
2012-02-03 17:15:24 +0000

    Clean up #includes and remove dead code.

--- qt/qtclient.cc
+++ qt/qtclient.cc
@@ -1,48 +1,8 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <assert.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-
 #include "ami/qt/DetectorSelect.hh"
 #include "ami/qt/Path.hh"
-#include "ami/app/AmiApp.hh"
 #include "ami/service/Ins.hh"
 
 #include <QtGui/QApplication>
-
-using namespace Ami;
-
-#if 0
-class QtAmiApp {
-public:
-  static int start(string label, unsigned serverGroup, unsigned ppinterface = 0x7f000001, unsigned interface = 0x7f000001, string loadfile = "");
-  static unsigned parse_interface(char* iarg);
-
-};
-
-void QtAmiApp::start(std::string label, unsigned serverGroup, unsigned ppinterface, unsigned interface, std::string loadfile)
-{
-
-    FILE* f = fopen(loadfile.c_str(),"r");
-    if (f) {
-      const int MaxConfigSize = 0x100000;
-      char* buffer = new char[MaxConfigSize];
-      int size = fread(buffer,1,MaxConfigSize,f);
-      fclose(f);
-      select->set_setup(buffer,size);
-      delete[] buffer;
-    }
-    else {
-      fprintf(stderr, "Unable to open %s\n",loadfile.c_str());
-    }
-  }
-}
-#endif
 
 int main(int argc, char **argv) 
 {
@@ -53,13 +13,13 @@ int main(int argc, char **argv)
 
   for(int i=0; i<argc; i++) {
     if (strcmp(argv[i],"-I")==0) {
-      ppinterface = Ins::parse_interface(argv[++i]);
+      ppinterface = Ami::Ins::parse_interface(argv[++i]);
     }
     else if (strcmp(argv[i],"-i")==0) {
-      interface = Ins::parse_interface(argv[++i]);
+      interface = Ami::Ins::parse_interface(argv[++i]);
     }
     else if (strcmp(argv[i],"-s")==0) {
-      serverGroup = Ins::parse_ip(argv[++i]);
+      serverGroup = Ami::Ins::parse_ip(argv[++i]);
     }
     else if (strcmp(argv[i],"-f")==0) {
       Ami::Qt::Path::setBase(argv[++i]);
