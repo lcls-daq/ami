d39b9a88e268f28d15725dcdafff1fe668dde324
jbarrera <none@example.com>
2012-02-03 17:29:25 +0000

    Run AmiApp::run in a task, not a separate process. Run XtcFileClient inside DetectorSelect window instead of in a separate window.

--- qt/qtami.cc
+++ qt/qtami.cc
@@ -4,7 +4,10 @@
 #include "ami/qt/DetectorSelect.hh"
 #include "ami/qt/Path.hh"
 #include "ami/service/Ins.hh"
+#include "ami/service/Routine.hh"
+#include "ami/service/Task.hh"
 #include <QtGui/QApplication>
+#include <QtGui/QGroupBox>
 
 static void usage(char* progname) {
   fprintf(stderr,
@@ -15,6 +18,32 @@ static void usage(char* progname) {
 	  "         [-L <user plug-in path>]\n", progname);
 }
 
+class AmiRoutine : public Ami::Routine {
+private:
+  char* _partitionTag;
+  unsigned _serverGroup;
+  std::vector<char *>& _module_names;
+  unsigned _interface;
+
+public:
+  AmiRoutine(char* partitionTag,
+             unsigned serverGroup,
+             std::vector<char *>& module_names,
+             unsigned interface) :
+    _partitionTag(partitionTag),
+    _serverGroup(serverGroup),
+    _module_names(module_names),
+    _interface(interface) {
+  }
+
+  void routine() {
+    const int partitionIndex = 0;
+    const bool sync = true;
+    std::cout << "AmiApp::run starting..." << std::endl;
+    Ami::AmiApp::run(_partitionTag, _serverGroup, _module_names, _interface, partitionIndex, sync);
+    std::cout << "AmiApp::run done..." << std::endl;
+  }
+};
 
 int main(int argc, char* argv[]) {
   char* partitionTag = NULL;
@@ -52,17 +81,22 @@ int main(int argc, char* argv[]) {
     exit(0);
   }
 
-  QApplication app(argc, argv);
+  // Start AmiApp in the background.
+  Ami::Task* amiTask = new Ami::Task(Ami::TaskObject("AmiThread"));
+  AmiRoutine amiRoutine = AmiRoutine(partitionTag, serverGroup, module_names, interface);
+  amiTask->call(&amiRoutine);
 
-  Ami::Qt::XtcFileClient client(partitionTag, serverGroup, interface, dir);
-  client.show();
-  if (fork() == 0) {
-    const int partitionIndex = 0;
-    const bool sync = true;
-    Ami::AmiApp::run(partitionTag, serverGroup, module_names, interface, partitionIndex, sync);
-  }
-  Ami::Qt::DetectorSelect output("AMO Offline Monitoring", interface, interface, serverGroup);
+  // Start the DetectorSelect GUI.
+  QApplication app(argc, argv);
+  QGroupBox* groupBox = new QGroupBox("Offline");
+  Ami::Qt::DetectorSelect output("AMO Offline Monitoring", interface, interface, serverGroup, groupBox);
   output.show();
+
+  // Start the XtcFileClient inside of the DetectorSelect GUI.
+  Ami::Qt::XtcFileClient client(groupBox, partitionTag, serverGroup, interface, dir);
+  //client.show();
   app.exec();
+
+  amiTask->destroy();
   exit(0);
 }
