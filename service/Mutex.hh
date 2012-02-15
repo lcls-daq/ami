7751f888574446a41c7d434bef0b365c4ca51bde
jbarrera <none@example.com>
2012-02-15 18:10:24 +0000

    Added Mutex for monitor-style synchronization.

---
+++ service/Mutex.hh
@@ -0,0 +1,38 @@
+#ifndef PDS_MUTEX_HH
+#define PDS_MUTEX_HH
+
+#include <pthread.h>
+
+namespace Ami {
+  class Mutex {
+  public:
+    Mutex(const char* name);
+    ~Mutex();
+    void lock();
+    void unlock();
+  private:
+    const char* _name;
+    pthread_t _owner;
+    pthread_cond_t _condition;
+    pthread_mutex_t _mutex;
+  };
+}
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
