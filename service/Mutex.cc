16f09019e29c889b91ab3330962a97d267d0cee6
jbarrera <none@example.com>
2012-02-16 02:30:05 +0000

    Made quieter.

--- service/Mutex.cc
+++ service/Mutex.cc
@@ -4,6 +4,8 @@
  * Not recursive (thread cannot a mutex it already has locked).
  */
 
+#define DEBUG 0
+
 #include "Mutex.hh"
 #include <iostream>
 
@@ -37,12 +39,18 @@ void Mutex::lock() {
       cout << "Mutex::lock: " << _name << " already locked by " << pthread_self() << endl;
       fail();
     }
+#if DEBUG
     cout << "Mutex::lock: " << _name << ": waiting" << endl;
+#endif
     pthread_cond_wait(&_condition, &_mutex);
+#if DEBUG
     cout << "Mutex::lock: " << _name << ": trying again" << endl;
+#endif
   }
   _owner = pthread_self();
+#if DEBUG
   cout << "Mutex::lock: " << _name << " now locked by " << pthread_self() << endl;
+#endif
   pthread_mutex_unlock(&_mutex);
 }
 
@@ -53,7 +61,9 @@ void Mutex::unlock() {
     fail();
   }
   _owner = 0;
+#if DEBUG
   cout << "Mutex::unlock: " << _name << " unlocked by " << pthread_self() << endl;
+#endif
   pthread_cond_broadcast(&_condition);
   pthread_mutex_unlock(&_mutex);
 }
