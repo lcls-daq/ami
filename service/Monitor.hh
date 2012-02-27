5f458886dc7771b51f142cba383e0eda80ac12cc
jbarrera <none@example.com>
2012-02-27 22:54:50 +0000

    Rename Mutex to Monitor.

--- service/Monitor.hh
+++ service/Monitor.hh
@@ -1,17 +1,18 @@
-#ifndef PDS_MUTEX_HH
-#define PDS_MUTEX_HH
+#ifndef PDS_MONITOR_HH
+#define PDS_MONITOR_HH
 
 #include <pthread.h>
 
 namespace Ami {
-  class Mutex {
+  class Monitor {
   public:
-    Mutex(const char* name);
-    ~Mutex();
+    Monitor(const char* name, const bool debug = false);
+    ~Monitor();
     void lock();
     void unlock();
   private:
     const char* _name;
+    const bool _debug;
     pthread_t _owner;
     pthread_cond_t _condition;
     pthread_mutex_t _mutex;
