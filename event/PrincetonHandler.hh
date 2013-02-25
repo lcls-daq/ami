1dd4b53ec45dcb6e51228fd7d006cb0a8009af44
tomytsai <none@example.com>
2013-02-25 23:55:24 +0000

    *** empty log message ***

--- event/PrincetonHandler.hh
+++ event/PrincetonHandler.hh
@@ -18,20 +18,20 @@ namespace Ami {
     const Entry* entry(unsigned) const;
     void         reset();
   private:
-    virtual void _configure(Pds::TypeId type, const void* payload, const Pds::ClockTime& t);    
+    virtual void _configure(Pds::TypeId type, const void* payload, const Pds::ClockTime& t);
     virtual void _calibrate(const void* payload, const Pds::ClockTime& t);
     virtual void _event    (Pds::TypeId type, const void* payload, const Pds::ClockTime& t);
     virtual void _damaged  ();
   private:
     //PrincetonHandler(const Pds::DetInfo& info, const EntryImage*);
     PrincetonConfigType  _config;
-    FeatureCache&             _cache;
-    int                       _iCacheIndexTemperature;
-    EntryImage*               _entry;
+    FeatureCache&        _cache;
+    int                  _iCacheIndexTemperature;
+    EntryImage*          _entry;
 
     /*
      * These functions will never be called. The above _configure() and _event() replace these two.
-     */    
+     */
     virtual void _configure(const void* payload, const Pds::ClockTime& t);
     virtual void _event    (const void* payload, const Pds::ClockTime& t);
   };
