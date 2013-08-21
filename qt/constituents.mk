ead3543c10067d62455eb9b37f52aedb8a26f6e8
weaver <none@example.com>
2013-08-21 17:39:17 +0000

    *** empty log message ***

--- qt/constituents.mk
+++ qt/constituents.mk
@@ -186,7 +186,7 @@ libsrcs_amiblv := blv
 # List special include directories (if any) needed by lib_a as
 # <project>/<incdir>. Note that the top level release directory is
 # already in the search path.
-libincs_amiqt := $(qt_incs) qwt/include
+libincs_amiqt := $(qt_incs) qwt/include ndarray/include
 libsinc_amiqt := $(qwtsinc)
 
 # List system include directories (if any) needed by lib_a as <incdir>.
