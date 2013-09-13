dc609100e8d142051f8ca0f5b95bdde49d98f701
weaver <none@example.com>
2013-09-13 18:12:10 +0000

    Added missing library dependency for python interface

--- python/constituents.mk
+++ python/constituents.mk
@@ -6,6 +6,7 @@ libsrcs_pyami += Client.cc
 libincs_pyami := python/include/python2.5 pdsdata/include ndarray/include 
 libincs_pyami += ndarray/include boost/include
 liblibs_pyami := pdsdata/xtcdata pdsdata/psddl_pdsdata pdsdata/compressdata
+liblibs_pyami += pdsalg/pdsalg
 liblibs_pyami += ami/service ami/data ami/server ami/client
 liblibs_pyami += qt/QtCore
 libslib_pyami := $(USRLIBDIR)/rt gomp
