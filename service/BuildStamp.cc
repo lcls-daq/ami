dd17fee0897084bd1c64d9c9166a97061a72633a
weaver <none@example.com>
2013-09-24 16:37:39 +0000

    Add build tag and time into service library

---
+++ service/BuildStamp.cc
@@ -0,0 +1,11 @@
+#include "ami/service/BuildStamp.hh"
+
+const char* Ami::BuildStamp::time()
+{
+  return "AMI_BUILD_TIME";
+}
+
+const char* Ami::BuildStamp::tag ()
+{
+  return "AMI_BUILD_TAG";
+}
