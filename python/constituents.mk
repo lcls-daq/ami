3dd725a711180d88097dad9943d62c0cb599a73b
weaver <none@example.com>
2013-12-13 18:59:34 +0000

    *** empty log message ***

--- python/constituents.mk
+++ python/constituents.mk
@@ -7,6 +7,7 @@ libincs_pyami := python/include/python2.5 pdsdata/include ndarray/include boost/
 libincs_pyami += ndarray/include boost/include 
 liblibs_pyami := pdsdata/xtcdata pdsdata/psddl_pdsdata pdsdata/compressdata
 liblibs_pyami += pdsalg/pdsalg
+liblibs_pyami += gsl/gsl gsl/gslcblas
 liblibs_pyami += ami/amisvc ami/amidata ami/server ami/client
 liblibs_pyami += qt/QtCore
 libslib_pyami := $(USRLIBDIR)/rt gomp
