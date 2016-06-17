a401daa2c5a859d42a0dc5c968c849b4f271dd6e
ddamiani <ddamiani@SLAC.STANFORD.EDU>
2016-06-17 05:53:31 +0000

    fix naming bugs in EOrbitsHandler

--- event/EOrbitsHandler.cc
+++ event/EOrbitsHandler.cc
@@ -1,8 +1,5 @@
 #include "EOrbitsHandler.hh"
 
-#include "pdsdata/xtc/Dgram.hh"
-#include "pdsdata/xtc/Level.hh"
-#include "pdsdata/xtc/Xtc.hh"
 #include "pdsdata/xtc/BldInfo.hh"
 #include "pdsdata/psddl/bld.ddl.h"
 
@@ -26,7 +23,7 @@ void   EOrbitsHandler::_calibrate(Pds::TypeId, const void* payload, const Pds::C
 void   EOrbitsHandler::_configure(Pds::TypeId id, const void* payload, const Pds::ClockTime& t)
 {
   const char* name = "BLD:EORBITS";
-  char* buffer = new char[strlen(name)+15];
+  char* buffer = new char[strlen(name)+14];
   strcpy(buffer,name);
   char* iptr = buffer+strlen(buffer);
 
@@ -34,11 +31,11 @@ void   EOrbitsHandler::_configure(Pds::TypeId id, const void* payload, const Pds
   case 0:
     { const Pds::Bld::BldDataEOrbitsV0& d = *reinterpret_cast<const Pds::Bld::BldDataEOrbitsV0*>(payload);
       for(unsigned i=0; i<d.nBPMS(); i++) {
-        sprintf(iptr,":BPMX:CH%d",i);
+        sprintf(iptr,":BPM-%03d:X",i+1);
         _add_to_cache(buffer);
-        sprintf(iptr,":BPMY:CH%d",i);
+        sprintf(iptr,":BPM-%03d:Y",i+1);
         _add_to_cache(buffer);
-        sprintf(iptr,":BPMTMIT:CH%03d",i);
+        sprintf(iptr,":BPM-%03d:TMIT",i+1);
         _add_to_cache(buffer);
       }
     } break;
@@ -77,22 +74,22 @@ unsigned     EOrbitsHandler::nentries() const { return 0; }
 const Entry* EOrbitsHandler::entry   (unsigned) const { return 0; }
 void         EOrbitsHandler::rename  (const char* name)
 {
-  char* buffer = new char[strlen(name)+15];
-  strcpy(buffer,name);
+  char* buffer = new char[4+strlen(name)+14];
+  sprintf(buffer, "BLD:%s", name);
   char* iptr = buffer+strlen(buffer);
 
   for(unsigned i=0; i<_indices.size(); i++) {
     switch(i % 3) {
     case 0:
-      sprintf(iptr,":BPMX:CH%d",i);
+      sprintf(iptr,":BPM-%03d:X",i/3+1);
       _rename_cache(_indices[i],buffer);
       break;
     case 1:
-      sprintf(iptr,":BPMY:CH%d",i);
+      sprintf(iptr,":BPM-%03d:Y",i/3+1);
       _rename_cache(_indices[i],buffer);
       break;
     case 2:
-      sprintf(iptr,":BPMTMIT:CH%03d",i);
+      sprintf(iptr,":BPM-%03d:TMIT",i/3+1);
       _rename_cache(_indices[i],buffer);
       break;
     }
