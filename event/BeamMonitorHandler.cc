50b4f4ca4df931abfc275feada9acd122ecbe959
ddamiani <ddamiani@SLAC.STANFORD.EDU>
2016-10-18 00:18:39 +0000

    Update to deal with changes to BldDataBeamMonitorV1

--- event/BeamMonitorHandler.cc
+++ event/BeamMonitorHandler.cc
@@ -24,18 +24,17 @@ void   BeamMonitorHandler::_calibrate(Pds::TypeId, const void* payload, const Pd
 void   BeamMonitorHandler::_configure(Pds::TypeId id, const void* payload, const Pds::ClockTime& t)
 {
   char buffer[64];
-  strncpy(buffer,Pds::BldInfo::name(static_cast<const Pds::BldInfo&>(info())),54);
+  strncpy(buffer,Pds::BldInfo::name(static_cast<const Pds::BldInfo&>(info())),51);
   char* iptr = buffer+strlen(buffer);
 
   switch(id.version()) {
-  case 0:
+  case 1:
     sprintf(iptr,":SUM");         _add_to_cache(buffer);
     sprintf(iptr,":XPOS");        _add_to_cache(buffer);
     sprintf(iptr,":YPOS");        _add_to_cache(buffer);
-    sprintf(iptr,":PEAK:AMP");    _add_to_cache(buffer);
-    sprintf(iptr,":PEAK:POS");    _add_to_cache(buffer);
     for(unsigned i=0; i<Pds::Bld::BldDataBeamMonitorV1::NCHANNELS; i++) {
-      sprintf(iptr,":CH%02d",i);  _add_to_cache(buffer);
+      sprintf(iptr,":PEAK_A:CH%02d",i);  _add_to_cache(buffer);
+      sprintf(iptr,":PEAK_T:CH%02d",i);  _add_to_cache(buffer);
     }
     break;
   default:
@@ -47,17 +46,17 @@ void   BeamMonitorHandler::_event    (Pds::TypeId id, const void* payload, const
 {
   unsigned index = _index;
   switch(id.version()) {
-  case 0:
+  case 1:
     { const Pds::Bld::BldDataBeamMonitorV1& d =
         *reinterpret_cast<const Pds::Bld::BldDataBeamMonitorV1*>(payload);
       _cache.cache(index++, d.TotalIntensity());
       _cache.cache(index++, d.X_Position());
       _cache.cache(index++, d.Y_Position());
-      _cache.cache(index++, d.peakA());
-      _cache.cache(index++, d.peakT());
-      ndarray<const double, 1> chan_intesity = d.Channel_Intensity();
+      ndarray<const double, 1> peakA = d.peakA();
+      ndarray<const unsigned short, 1> peakT = d.peakT();
       for(unsigned i=0; i<Pds::Bld::BldDataBeamMonitorV1::NCHANNELS; i++) {
-        _cache.cache(index++, chan_intesity[i]);
+        _cache.cache(index++, peakA[i]);
+        _cache.cache(index++, peakT[i]);
       }
     } break;
   default:
@@ -71,18 +70,16 @@ const Entry* BeamMonitorHandler::entry   (unsigned) const { return 0; }
 void         BeamMonitorHandler::rename  (const char* name)
 {
   char buffer[64];
-  strncpy(buffer,name,54);
+  strncpy(buffer,name,51);
   char* iptr = buffer+strlen(buffer);
 
-  unsigned i=_index;
+  unsigned index=_index;
 
-  sprintf(iptr,":SUM");       _rename_cache(i++,buffer);
-  sprintf(iptr,":XPOS");      _rename_cache(i++,buffer);
-  sprintf(iptr,":YPOS");      _rename_cache(i++,buffer);
-  sprintf(iptr,":PEAK:AMP");  _rename_cache(i++,buffer);
-  sprintf(iptr,":PEAK:POS");  _rename_cache(i++,buffer);
-
-  for(; i<_indices.size(); i++) {
-    sprintf(iptr,":CH%02d",i); _rename_cache(i,buffer);
+  sprintf(iptr,":SUM");       _rename_cache(index++,buffer);
+  sprintf(iptr,":XPOS");      _rename_cache(index++,buffer);
+  sprintf(iptr,":YPOS");      _rename_cache(index++,buffer);
+  for(unsigned i=0; index<_indices.size()-1; i++) {
+    sprintf(iptr,":PEAK_A:CH%02d",i); _rename_cache(index++,buffer);
+    sprintf(iptr,":PEAK_T:CH%02d",i); _rename_cache(index++,buffer);
   }
 }
