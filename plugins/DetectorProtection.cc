60ec15b46e48ffefdd172baf8f2f8fdc93448c88
Daniel Damiani <ddamiani@slac.stanford.edu>
2023-10-12 11:58:18 -0700

    make detector protection file hidden

--- plugins/DetectorProtection.cc
+++ plugins/DetectorProtection.cc
@@ -35,7 +35,7 @@ DetectorProtection::DetectorProtection(const char* name, const char* short_name)
   _sname(short_name),
   _alias_ready(false),
   _name_service(new NameService),
-  _threshold(new Threshold("DetectorProtection.cfg", _name_service))
+  _threshold(new Threshold(".DetectorProtection.cfg", _name_service))
 {
   SEVCHK ( ca_context_create(ca_enable_preemptive_callback ),
            "detprotect calling ca_context_create" );
